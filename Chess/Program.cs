c73ae47013eb2b7e90a2fd35b4f5f0523dd28da1
Morgan Hopkins <106188591@student.swin.edu.au>
2026-05-01 00:02:33 +1000

    splashkitting, blank window

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SplashKitSDK;
+
+namespace Chess;
+
+public class Program
+{
+    public static void Main()
+    {
+        Window window = new Window("MAIchess", 480, 480);
+
+        while (!window.CloseRequested)
+        {
+            SplashKit.ProcessEvents();
+        }
+    }
+}
