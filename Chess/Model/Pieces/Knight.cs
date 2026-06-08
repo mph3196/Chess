78e75ac41b48a4394c0718e0a412a9f4dd326c5c
Morgan Hopkins <106188591@student.swin.edu.au>
2026-06-08 22:01:44 +1000

    rook can move

--- Chess/Model/Pieces/Knight.cs
+++ Chess/Model/Pieces/Knight.cs
@@ -12,6 +12,21 @@ public class Knight : Piece
     public override List<Move> GetLegalMoves(int rank, BoardFile file, List<Square> squares, SquareLookup lookup)
     {
         List<Move> legalMoves = new List<Move>();
+
+        foreach (Square s in squares)
+        {
+            int rankDiff = Math.Abs(s.Rank - rank);
+            int fileDiff = Math.Abs((int)s.File - (int)file);
+
+            if ((rankDiff == 2 && fileDiff == 1) || (rankDiff == 1 && fileDiff == 2))
+            {
+                if (!s.Occupied || (s.Occupied && s.Occupant.Color != this.Color))
+                {
+                    legalMoves.Add(new Move(rank, file, s.Rank, s.File));
+                }
+            }
+        }
+
         return legalMoves;
     }
     
