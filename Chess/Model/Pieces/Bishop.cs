5663a6964d7ea8786ed13b62a717a84cb1afbb84
Morgan Hopkins <106188591@student.swin.edu.au>
2026-06-08 20:10:50 +1000

    bishop working? move on to turn management maybe

--- Chess/Model/Pieces/Bishop.cs
+++ Chess/Model/Pieces/Bishop.cs
@@ -12,6 +12,47 @@ public class Bishop : Piece
     public override List<Move> GetLegalMoves(int rank, BoardFile file, List<Square> squares)
     {
         List<Move> legalMoves = new List<Move>();
+        var squareMap = new Dictionary<(int Rank, BoardFile File), Square>();
+        foreach (Square s in squares)
+        {
+            squareMap[(s.Rank, s.File)] = s;
+        }
+
+        foreach (Square s in squares)
+        {
+            int rankDiff = Math.Abs(s.Rank - rank);
+            int fileDiff = Math.Abs((int)s.File - (int)file);
+            if (rankDiff == fileDiff && rankDiff > 0)
+            {
+                int rankStep = s.Rank > rank ? 1 : -1;
+                int fileStep = (int)s.File > (int)file ? 1 : -1;
+
+                int r = rank + rankStep;
+                int f = (int)file + fileStep;
+                bool blocked = false;
+
+                while (r != s.Rank || f != (int)s.File)
+                {
+                    if (squareMap.TryGetValue((r, (BoardFile)f), out Square? travelled) && travelled.Occupied)
+                    {
+                        blocked = true;
+                        break;
+                    }
+                    r += rankStep;
+                    f += fileStep;
+                }
+
+                if (!blocked)
+                {
+                    if (!s.Occupied || (s.Occupied && s.Occupant.Color != this.Color))
+                    {
+                        legalMoves.Add(new Move(rank, file, s.Rank, s.File));
+                    }
+                }
+            }
+
+        }
+
         return legalMoves;
     }
     
